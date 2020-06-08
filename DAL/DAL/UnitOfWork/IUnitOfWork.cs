02e7e2b169f628f9960e44ac5d2de31883a07038
denysgashaw <denys.gashaw@gmail.com>
2020-06-08 06:18:55 +0300

    Added IUnitOfWork

---
+++ DAL/DAL/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.UnitOfWork
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICartItemRepository cartitems { get; }
+        ICustomerRepository customers { get; }
+        ICartRepository carts { get; }
+        IStoreOrderRepository storeorders { get; }
+        void Save();
+
+    }
+}
