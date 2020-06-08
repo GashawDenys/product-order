535bdeb0a67c9dbee6d69287dbc01881fdeec43b
denysgashaw <denys.gashaw@gmail.com>
2020-06-08 05:55:14 +0300

    Added product_orderContext

---
+++ DAL/DAL/EF/product_orderContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Entities;
+
+namespace DAL.EF
+{
+    public class categoryContext
+        : DbContext
+    {
+        public DbSet<cart> Carts { get; set; }
+        public DbSet<customer> Customers { get; set; }
+        public DbSet<cartItem> CartItems { get; set; }
+        public DbSet<storeOrder> StoreOrders { get; set; }
+        public categoryContext(DbContextOptions options)
+            : base(options) { }
+    }
+}
